a36a9b7baf1152155e68430011a46270e5fd5dd8
demon <651939413@qq.com>
2017-03-19 21:34:35 +0800

    数组操作的基本函数

--- 002-数组函数/002-数组的分段和填充.php
+++ 002-数组函数/002-数组的分段和填充.php
@@ -7,12 +7,12 @@ $arr = [
     'five'  =>5,
 ];
 //1、数组的分段
-//$arr1 = array_slice($arr,0,3);               //可以将数组中的一段取出，此函数忽略键名
-//var_dump($arr1);                             //array(3) { ["one"]=> int(1) ["two"]=> int(2) ["three"]=> int(3) }
-//
-//$arr2 = array_splice($arr,0,3,array("one")); //可以将数组中的一段取出，与上个函数不同在于返回的序列从原数组中删除
-//var_dump($arr2);                             //array(3) { ["one"]=> int(1) ["two"]=> int(2) ["three"]=> int(3) }
-//var_dump($arr);                              //array(3) { [0]=> string(3) "one" ["four"]=> int(4) ["five"]=> int(5) }
+$arr1 = array_slice($arr,0,3);               //可以将数组中的一段取出，此函数忽略键名
+var_dump($arr1);                             //array(3) { ["one"]=> int(1) ["two"]=> int(2) ["three"]=> int(3) }
+
+$arr2 = array_splice($arr,0,3,array("one")); //可以将数组中的一段取出，与上个函数不同在于返回的序列从原数组中删除
+var_dump($arr2);                             //array(3) { ["one"]=> int(1) ["two"]=> int(2) ["three"]=> int(3) }
+var_dump($arr);                              //array(3) { [0]=> string(3) "one" ["four"]=> int(4) ["five"]=> int(5) }
 
 //2、分割多个数组
 $arr3 = array_chunk($arr,3,TRUE);                       //可以将一个数组分割成多个，TRUE为保留原数组的键名

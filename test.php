58aadc1ab72258acb4c378f1229568985ca848f1
demon <651939413@qq.com>
2017-03-20 14:15:25 +0800

    crul库

--- test.php
+++ test.php
@@ -1,15 +1,15 @@
 <?php
-//1.初始化，创建一个新cURL资源
-$ch = curl_init();
+    //1.初始化，创建一个新cURL资源
+    $ch = curl_init();
+    //2.设置URL和相应的选项
+    curl_setopt($ch, CURLOPT_URL, "http://www.baidu.com/");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);  //设置后则不再进行输出,不在浏览器进行输出，转为文件流的形式
+    //3.抓取URL并把它传递给浏览器
+    $data = curl_exec($ch);
+    //4.关闭cURL资源，并且释放系统资源
+    curl_close($ch);
 
-//2.设置URL和相应的选项
-curl_setopt($ch, CURLOPT_URL, "http://www.baidu.com/");
-curl_setopt($ch, CURLOPT_HEADER, 0);
+    file_put_contents('./curl.html',$data);
 
-//3.抓取URL并把它传递给浏览器
-$res = curl_exec($ch);
 
-//4.关闭cURL资源，并且释放系统资源
-curl_close($ch);
-
-var_dump($res);

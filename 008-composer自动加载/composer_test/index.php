957c186517438ae31bd1ef7dd09ab816aea57ad2
demon <651939413@qq.com>
2017-03-25 22:42:17 +0800

    深入学习composer自动加载

---
+++ 008-composer自动加载/composer_test/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuxiang
+ * Date: 16/5/12
+ * Time: 19:59
+ */
+require_once __DIR__.'/vendor/autoload.php';
+
+$container = new Illuminate\Container\Container();
+echo '<pre/>';
+var_dump($container);
